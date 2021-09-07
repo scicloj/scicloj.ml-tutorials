316c191f544bee938a9c0a40b90e91b601da61a4
Carsten Behring <carsten.behring@gmail.com>
2021-09-07 13:56:18 +0200

    fixed sytax

--- render_all.clj
+++ render_all.clj
@@ -1,23 +1,20 @@
 (ns render-all
-  (:require [notespace.cli :as cli]
-            ;; [scicloj.ml.intro]
-            )
-  )
+  (:require [notespace.cli :as cli]))
+
+  
 (def nss ['scicloj.ml.intro
           'scicloj.ml.advanced
           'scicloj.ml.models
           'scicloj.ml.sklearnclj
           'scicloj.ml.transformers
           'scicloj.ml.third-party
-          'scicloj.ml.titanic
-          ]
+          'scicloj.ml.titanic])
 
-  )
 
 (run!
  #(cli/eval-and-render-a-notespace {:ns %})
- nss
- )
+ nss)
+ 
 
 
 (ns-publics 'scicloj.ml.intro)
