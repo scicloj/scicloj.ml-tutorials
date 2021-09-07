fb203d64508376339a53194dfa97e3f2908d957f
Carsten Behring <carsten.behring@gmail.com>
2021-09-07 10:24:52 +0200

    added script to render all tutorials

---
+++ render_all.clj
@@ -0,0 +1,24 @@
+(ns render-all
+  (:require [notespace.cli :as cli]
+            ;; [scicloj.ml.intro]
+            )
+  )
+(def nss ['scicloj.ml.intro
+          'scicloj.ml.advanced
+          'scicloj.ml.models
+          'scicloj.ml.sklearnclj
+          'scicloj.ml.transformers
+          'scicloj.ml.third-party
+          'scicloj.ml.titanic
+          ]
+
+  )
+
+(run!
+ #(cli/eval-and-render-a-notespace {:ns %})
+ nss
+ )
+
+
+(ns-publics 'scicloj.ml.intro)
+(System/exit 0)
