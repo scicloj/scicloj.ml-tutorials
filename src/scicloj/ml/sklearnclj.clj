9acd7089ec78f67c5baea822887b9df972c8df34
Carsten Behring <carsten.behring@gmail.com>
2021-11-04 13:31:22 +0100

    added TOC
    cleaned up

--- src/scicloj/ml/sklearnclj.clj
+++ src/scicloj/ml/sklearnclj.clj
@@ -3,8 +3,8 @@
    [notespace.api :as note]
    [notespace.kinds :as kind]
    [scicloj.sklearn-clj.ml]
-   [scicloj.ml.ug-utils :refer :all]
-   ))
+   [scicloj.ml.ug-utils :refer :all]))
+   
 
 
 
@@ -13,13 +13,13 @@
   (note/eval-this-notespace)
   (note/reread-this-notespace)
   (note/render-static-html "docs/userguide-sklearnclj.html")
-  (note/init)
+  (note/init))
 
-  )
+  
 ["# sklearn-clj"]
 
 ["The scicloj.ml plugin [sklearn-clj](https://github.com/scicloj/sklearn-clj)
- gives easy access to all models from [scikit-learn](https://scikit-learn.org/stable/)" ]
+ gives easy access to all models from [scikit-learn](https://scikit-learn.org/stable/)"]
 
 ["After [libpython.clj](https://github.com/clj-python/libpython-clj)
  has been setup with the python package sklearn installed,
@@ -31,68 +31,53 @@ the following lines show how to use any sklearn model in a usual scicloj.ml pipe
          '[tech.v3.dataset.tensor :as dst]
          '[scicloj.sklearn-clj]
          '[scicloj.metamorph.ml.toydata :as toydata]
-         )
+         '[libpython-clj2.python :refer [py.-] :as py])
+         
 
-(def ds (-> (dst/tensor->dataset [[0 0 0 ] [1 1 1 ] [2 2 2]])
+["Example: logistic regression"]
 
+(def ds (dst/tensor->dataset [[0 0 0 ] [1 1 1 ] [2 2 2]]))
 
-
-            ))
+["Make pipe with sklearn model 'logistic-regression'"]
 (def pipe
   (ml/pipeline
    (mm/set-inference-target 2)
+   {:metamorph/id :model}
    (mm/model {:model-type :sklearn.classification/logistic-regression
-              :max-iter 100
-              })))
+              :max-iter 100})))
 
-(pipe {:metamorph/data ds
-       :metamorph/mode :fit})
-
-["SVM"]
-
-(def iris-split
-  (->
-   (toydata/iris-ds)
-   (ds/split->seq  :holdout))
-  )
-
-
-(def pipe-fn
-  (ml/pipeline
-   ;; {:metamorph/id :model}
-   (mm/model {:model-type
-              :sklearn.classification/svc})
-   )
-
-  )
-
+["Train model"]
 (def fitted-ctx
-  (ml/fit-pipe
-   (:train (first  iris-split))
-   pipe-fn))
+  (pipe {:metamorph/data ds
+         :metamorph/mode :fit}))
+
+["Predict on new data"]
+(->
+ (ml/transform-pipe
+  (dst/tensor->dataset [[3 4 5]])
+  pipe
+  fitted-ctx)
+ :metamorph/data)
+
+["Access model details via python interop (libpython-clj)"]
+(-> fitted-ctx :model :model-data
+    (py.- coef_)
+    (py/->jvm))
 
 
 
-(ml/transform-pipe
- (:test (first  iris-split))
- pipe-fn
- fitted-ctx
- )
-
-(def iris
-  (ds/dataset
-   "https://raw.githubusercontent.com/scicloj/metamorph.ml/main/test/data/iris.csv" {:key-fn keyword})
-  )
-^kind/vega
-;; (surface-plot iris [:sepal_width :sepal_length]
-;;               {:model-type
-;;                :sklearn.classification/svc})
-
-
-
-
-
-
+(def attributes
+  (->> fitted-ctx :model :model-data (py/dir)
+       (filter #(and  (clojure.string/ends-with? % "_")
+                      (not (clojure.string/starts-with? % "_"))))))
+(ml/explain)
+["All model attributes:"]
+(map
+ (fn [attr]
+   (hash-map (keyword attr)
+             (-> fitted-ctx :model :model-data
+                 (py/get-attr attr))))
+ attributes)
 
 
 ["# Models"]
@@ -100,8 +85,22 @@ the following lines show how to use any sklearn model in a usual scicloj.ml pipe
 ["Below all models are listed with their parameters and the original documentation.
 
 The parameters are given as Clojure keys in kebap-case. As the document texts are imported from python
-they refer to the python spelling of the parameter. But the translation between the two should be obvious."
-]
+they refer to the python spelling of the parameter. But the translation between the two should be obvious."]
+
+^kind/hiccup-nocode
+[:ul
+
+
+ (->>
+  (ml/model-definition-names)
+  (filter #(contains? #{"sklearn.classification"
+                        "sklearn.regression"}
+
+                     (namespace %)))
+  sort
+  (map
+   #(vector :li [:a {:href (str "#" (str %))} (str %)])))]
+  
 
 
 
